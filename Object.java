c8e59681376179a6b9a65c3df691c2fbbf940e84
jackdek11 <55926128+jackdek11@users.noreply.github.com>
2020-10-14 11:05:27 +0200

    Update Object.java
    The Object class now has correct circlefy and checkCircle methods

--- Object.java
+++ Object.java
@@ -4,13 +4,13 @@ public class Object {
     public ArrayList<Pixel> pixels;
     int longestPixelChain;
     boolean isCircle,isbuilding;
-    int CircleAreaWithGivenRadius;
+    double CircleAreaWithGivenRadius;
     double CirclePerimeterWithGivenRadius;
 
     public Object(){
         isCircle=false;
         isbuilding=false;
-        CircleAreaWithGivenRadius=0;
+        CircleAreaWithGivenRadius=0.0;
         longestPixelChain=0;
         pixels = new ArrayList<Pixel>();
         CirclePerimeterWithGivenRadius=0.0;
@@ -45,14 +45,14 @@ public class Object {
         return false;
     }
 
-    public int isCircle(){
-        int radius = getLongestPixelChain()/2;
+    public double isCircle(){
+        double radius = getLongestPixelChain()/2;
         return radius;
     }
 
-    public int getSurfaceArea(){
-        int radius = isCircle();
-        CircleAreaWithGivenRadius = (int)(Math.PI*(radius*radius));
+    public double getSurfaceArea(){
+        double radius = isCircle();
+        CircleAreaWithGivenRadius = (Math.PI*(radius*radius));
         return CircleAreaWithGivenRadius;
     }
 
@@ -61,24 +61,19 @@ public class Object {
         CirclePerimeterWithGivenRadius = (2*Math.PI*radius);
         return CirclePerimeterWithGivenRadius;
     }
-    
+
     public Circle circlefy(){
         Circle toCircle = new Circle(pixels);
-        return toCircle
+        return toCircle;
     }
-
 
     public boolean checkCircle(){
         //System.out.println("Meant to be: "+ getSurfaceArea()+"     | is: "+pixels.size());
-        if(getSurfaceArea()!=0&&pixels.size()/getSurfaceArea()>=1) {
+        if(getSurfaceArea()!=0&&(pixels.size()/getSurfaceArea())>=1) {
             isCircle = true;
             return true;
         }
-
         return false;
-        //1.38
-        //1.19
-        //1.11
     }
 
 
