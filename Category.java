0f5daa46f72de87a887da96c94e732e9f4f69b5f
Amy Solomons <slmamy003@cs.uct.ac.za>
2020-10-14 23:03:56 +0200

    Changed category class by removing arrays of objects for different colors and creating single arrayList as discussed with Rifqah

--- Category.java
+++ Category.java
@@ -1,41 +1,59 @@
 public class Category{
 	
-	public String colour;
-	public int min, max;
-	public Object[] reds, blues, greens;
+	private String colour;
+	private int numberOfDisks, min, max, shortestRadius, longestRadius;
+	// public Object[] reds, blues, greens;
+	private ArrayList<Circle> circlesInCategory;
 		
-	public Category(String colour, int min, int max, Object[] reds, Object[] blues, Object[] greens) {
+	public Category(String colour, int min, int max) {
 			this.colour = colour;
 			this.min = min;
 			this.max = max;
-			this.reds = reds;
-			this.blues = blues;
-			this.greens = greens;
 	}
 	
 	public String getCategoryColour() {
 		return colour;
 	}
-	
-	public int getMin() {
+
+	public int getMin(){
 		return min;
 	}
-	
+
 	public int getMax(){
 		return max;
 	}
-	
-	public Object[] getReds(){
-		return reds;
+
+	public void addCircleToCat(Circle cir){
+		circlesInCategory.add(cir);
+	}
+
+	public ArrayList<Circle> getDisksIntCategory(){
+		return circlesInCategory;
 	}
 	
-	public Object[] getBlues(){
-		return blues;
+	public int getNumberOfDisksInCat(){
+		return circlesInCategory.size();
 	}
 	
-	public Object[] getGreens(){
-		return greens;
-	}	
+	// public int getShortest() {
+	// 	return shortestRadius
+	// }
+	
+	// public int getLongest(){
+	// 	return longestRadius;
+	// }
+	
+	// public Object[] getReds(){
+	// 	return reds;
+	// }
+	
+	// public Object[] getBlues(){
+	// 	return blues;
+	// }
+	
+	// public Object[] getGreens(){
+	// 	return greens;
+	// }	
 		
 	/*
 	 * public void changeColour(int Category){ for(int i=0;i<pixels.size();i++){
