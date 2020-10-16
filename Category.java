f77f0c5a43c93cdb9417a25475b48d6152bab058
rifqahfrancis <55393292+rifqahfrancis@users.noreply.github.com>
2020-10-16 02:12:52 +0200

    Comments added

--- Category.java
+++ Category.java
@@ -1,3 +1,11 @@
+/** 
+* This class sorts the identified discs according to which category of colour they belong to
+* @author Rifqah Francis
+* @author Amy Solomons
+* @version 1.0
+* @since 09/10/2020
+*/
+
 import java.util.ArrayList;
 public class Category{
 	
@@ -5,33 +13,63 @@ public class Category{
 	private int numberOfDisks, min, max, shortestRadius, longestRadius;
 	// public Object[] reds, blues, greens;
 	private ArrayList<Circle> circlesInCategory;
-		
+	
+	/**
+	* The constructor for the Category class
+	* @param colour This initializes the colour variable
+	* @param min This initializes the min variable
+	* @param max This initializes the max variable
+	*/
 	public Category(String colour, int min, int max) {
 			this.colour = colour;
 			this.min = min;
 			this.max = max;
 	}
 	
+	/** 
+	* This returns the category colour
+	* @return String Returns the category colour in the form of a string
+	*/
 	public String getCategoryColour() {
 		return colour;
 	}
 
+	/**
+	* This returns the minimum value for the category
+	* @return int Returns the minimum in the form of an integer
+	*/	
 	public int getMin(){
 		return min;
 	}
 
+	/**
+	* This returns the maximum value for the category
+	* @return int Returns the maximum in the form of an integer
+	*/		
 	public int getMax(){
 		return max;
 	}
 
+	/** 
+	* This method adds a circle to an arraylist according to the category
+	* @param cir The circle of type Circle to be added
+	*/
 	public void addCircleToCat(Circle cir){
 		circlesInCategory.add(cir);
 	}
-
+	
+	/**
+	* This method returns all the discs/circles in a particular category
+	* @return ArrayList<Circle> Returns all the discs in the category in the form of an arraylist of type Circle
+	*/
 	public ArrayList<Circle> getDisksIntCategory(){
 		return circlesInCategory;
 	}
 	
+	/**
+	/* This method returns the number of discs/circles in a particular category by calling the size() method on the circlesInCategory arrayliste
+	* @return int Returns the number of discs as an int
+	*/
 	public int getNumberOfDisksInCat(){
 		return circlesInCategory.size();
 	}
