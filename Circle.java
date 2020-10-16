9fb58261fa036c1948518ac6c7246cb952c73a5a
rifqahfrancis <55393292+rifqahfrancis@users.noreply.github.com>
2020-10-16 02:44:56 +0200

    Comments added

--- Circle.java
+++ Circle.java
@@ -1,3 +1,9 @@
+/** 
+* This class changes the colours of various circles according to which category they belong to
+* @author Rifqah Francis
+* @version 1.0
+* @since 28/08/2020
+*/
 import java.util.ArrayList;
 import java.awt.*;
 
@@ -6,10 +12,18 @@ public class Circle{
     public ArrayList<Pixel> pixels;
     private Color color;
     
+    /**
+    * The constructor of the Circle class
+    * @param pixels Initializes the pixels variable
+    */
     public Circle(ArrayList<Pixel> pixels) {
 		 this.pixels = pixels;
 	}
 
+    /**
+    * This method changes the colour of each pixel of a circle according to which category they belong to
+    * @param Category This is the category number
+    */
     public void changeColour(int Category){
         for(int i=0;i<pixels.size();i++){
             if(Category==0){
@@ -24,6 +38,10 @@ public class Circle{
         }
     }
 
+    /**
+    * This method returns the category colour
+    * @return int Returns the category colour in the form of an integer
+    */
     public int getCategoryColor(){
         return color.getRGB();
     }
@@ -39,6 +57,11 @@ public class Circle{
 //             changeColour(2);
 //         }
     // }
+    
+      /**
+      * This method sets the colours of discs/circles in a particular category
+      * @param cat This is an array of type Category with all the discs that needs their colour set
+      */
       public void setColor(Category[] cat){
             for(Category c : cat){
                 if (c.getMin <= radius){
@@ -51,6 +74,10 @@ public class Circle{
         color = Color.YELLOW; //circle does not fall into category
       }
 
+      /**
+      * This method sets the method to the longest pixel chain
+      * @param longestPixelChain This is used to set to radius
+      */
       public void setLongestPixelChain(int longestPixelChain){
             radius = longestPixelChain;
       }
